4718149c477a34477a1b7c5e7288cac2a776d8b6
XavierTwitty <x.twitty@gmail.com>
2023-02-06 12:42:01 -0500

    components with routes

--- cleanauto/src/App.js
+++ cleanauto/src/App.js
@@ -1,10 +1,29 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Header from "./Header";
+import Home from "./Home";
+import Navigation from "./Navigation";
+import Services from "./Services";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-      <div></div>
+      <Header />
+      <Switch>
+        <Route>
+          <Home />
+        </Route>
+        <Route>
+          <Navigation />
+        </Route>
+        <Route>
+          <Services />
+        </Route>
+        <Route>
+          <Footer />
+        </Route>
+      </Switch>
     </div>
   );
 }
