48278fd1590e07831d335b797f6a02616adf76ce
XavierTwitty <x.twitty@gmail.com>
2023-02-07 11:18:44 -0500

    wip

--- cleanauto/src/App.js
+++ cleanauto/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Services from "./components/Services";
 import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
