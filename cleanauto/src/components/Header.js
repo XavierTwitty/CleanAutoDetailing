e7bac46074bc35262fc6af889a54d3bc84ca91c9
XavierTwitty <x.twitty@gmail.com>
2023-02-07 11:25:39 -0500

    logo

--- cleanauto/src/components/Header.js
+++ cleanauto/src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navigation from "./Navigation";
+import logo from "../photos/Clean-Auto-logos.jpeg";
 
 const Header = () => {
   return (
     <div className="header-container">
-      <div className="logos"></div>
+      {/* <div className="logos"></div> */}
+      <img src={logo} alt="logo" />
       <Navigation />
     </div>
   );
