3e6c53d2645e9ddb09434a9835ba742ef6b619f2
XavierTwitty <x.twitty@gmail.com>
2023-02-07 10:59:52 -0500

    components routes

--- cleanauto/src/App.js
+++ cleanauto/src/App.js
@@ -1,29 +1,30 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Header from "./Header";
-import Home from "./Home";
-import Navigation from "./Navigation";
-import Services from "./Services";
-import Footer from "./Footer";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Services from "./components/Services";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Switch>
-        <Route>
-          <Home />
-        </Route>
-        <Route>
-          <Navigation />
-        </Route>
-        <Route>
+      <Router>
+        <Fragment>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/footer" element={<Footer />} />
+
+            {/* <Route path="/services">
           <Services />
         </Route>
-        <Route>
+        <Route path="/footer">
           <Footer />
-        </Route>
-      </Switch>
+        </Route> */}
+          </Routes>
+        </Fragment>
+      </Router>
     </div>
   );
 }
