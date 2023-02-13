dbaa7a85f86bd5aadfcbac4a4cb21184796a1cf8
XavierTwitty <x.twitty@gmail.com>
2023-02-13 12:40:51 -0500

    photo gallery

--- cleanauto/src/components/Gallery.js
+++ cleanauto/src/components/Gallery.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  return (
+    <div>
+      <h2>Gallery</h2>
+
+      <img src={require("../photos/IMG_2151.jpg")}></img>
+      <img src={require("../photos/IMG_2152 2.jpg")}></img>
+      <img src={require("../photos/IMG_2170.jpg")}></img>
+      <img src={require("../photos/IMG_2242.jpg")}></img>
+      <img src={require("../photos/IMG_2246.jpg")}></img>
+      <img src={require("../photos/IMG_2243.jpg")}></img>
+      <img src={require("../photos/IMG_2389.jpg")}></img>
+      <img src={require("../photos/IMG_2391.jpg")}></img>
+      <img src={require("../photos/IMG_2394 5.jpg")}></img>
+    </div>
+  );
 };
 
 export default Gallery;
