d4335f7084e0fd8678a69fba24ded552c4fad480
XavierTwitty <x.twitty@gmail.com>
2023-02-13 12:44:56 -0500

    wip

--- cleanauto/src/components/Gallery.js
+++ cleanauto/src/components/Gallery.js
@@ -4,16 +4,17 @@ const Gallery = () => {
   return (
     <div>
       <h2>Gallery</h2>
-
-      <img src={require("../photos/IMG_2151.jpg")}></img>
-      <img src={require("../photos/IMG_2152 2.jpg")}></img>
-      <img src={require("../photos/IMG_2170.jpg")}></img>
-      <img src={require("../photos/IMG_2242.jpg")}></img>
-      <img src={require("../photos/IMG_2246.jpg")}></img>
-      <img src={require("../photos/IMG_2243.jpg")}></img>
-      <img src={require("../photos/IMG_2389.jpg")}></img>
-      <img src={require("../photos/IMG_2391.jpg")}></img>
-      <img src={require("../photos/IMG_2394 5.jpg")}></img>
+      <div className="gallery-container">
+        <img src={require("../photos/IMG_2151.jpg")}></img>
+        <img src={require("../photos/IMG_2152 2.jpg")}></img>
+        <img src={require("../photos/IMG_2170.jpg")}></img>
+        <img src={require("../photos/IMG_2242.jpg")}></img>
+        <img src={require("../photos/IMG_2246.jpg")}></img>
+        <img src={require("../photos/IMG_2243.jpg")}></img>
+        <img src={require("../photos/IMG_2389.jpg")}></img>
+        <img src={require("../photos/IMG_2391.jpg")}></img>
+        <img src={require("../photos/IMG_2394 5.jpg")}></img>
+      </div>
     </div>
   );
 };
