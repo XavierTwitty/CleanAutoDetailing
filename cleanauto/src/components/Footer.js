a9b581df0e7cc30270bccffa6da454d6fe773a2a
XavierTwitty <x.twitty@gmail.com>
2023-02-13 14:37:10 -0500

    react icon

--- cleanauto/src/components/Footer.js
+++ cleanauto/src/components/Footer.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { BsInstagram } from "react-icons/bs";
 
 const Footer = () => {
-  return <div className="footer-container">Footer</div>;
+  return (
+    <div className="footer-container">
+      <h4> Contact us :</h4>
+      <p>cleanautodetailing704@gmail.com</p>
+      <BsInstagram />
+    </div>
+  );
 };
 
 export default Footer;
