63855ad8eec2488f5dac52d96716691c768771d7
XavierTwitty <x.twitty@gmail.com>
2023-02-13 19:32:06 -0500

    ig icon

--- cleanauto/src/components/Footer.js
+++ cleanauto/src/components/Footer.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BsInstagram } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <h4> Contact us :</h4>
       <p>cleanautodetailing704@gmail.com</p>
-      <BsInstagram />
+      <h5>Follow us on IG!</h5>
+      <Link to="https://instagram.com/cleanautodetaling?igshid=YmMyMTA2M2Y=">
+        <BsInstagram color="white" />
+      </Link>
     </div>
   );
 };
