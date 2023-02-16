cab2425bfab879b7b45c44332538a1e5f17cdf8a
XavierTwitty <x.twitty@gmail.com>
2023-02-16 09:41:09 -0500

    breakpoint styling

--- cleanauto/src/components/Home.js
+++ cleanauto/src/components/Home.js
@@ -9,21 +9,13 @@ const Home = () => {
       <h2> Professional mobile detaling </h2>
       <div className="interior-exterior-pic">
         <a href="/services">
-          <img
-            src={require("../photos/IMG_2387.jpg")}
-            alt="interior"
-            className="interior-exterior-img"
-          />
+          <img src={require("../photos/IMG_2387.jpg")} alt="interior" />
           <p className="pos-absolute"> Interior & Exterior</p>
         </a>
       </div>
       <div className="exterior-pic">
         <a href="/services">
-          <img
-            src={require("../photos/IMG_2246.jpg")}
-            alt="exterior"
-            className="exterior-img"
-          />
+          <img src={require("../photos/IMG_2246.jpg")} alt="exterior" />
           <p className="pos-abs"> Exterior </p>
         </a>
       </div>
